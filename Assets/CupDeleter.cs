0f780a67a6e6e5c83482d214446c7aabe194d0ac
Dorafanboy <romakremlev@mail.ru>
2022-05-03 14:41:25 +0700

    add movement and spawn cups
    need create spawn pole, and pole functions

---
+++ Assets/CupDeleter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CupDeleter : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Voshel");
+    }
+}
